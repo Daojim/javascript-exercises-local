db89bcf6ffc4fb7ba52a594196465ddaa5b9cc1e
Daojim <jimmyddao@gmail.com>
2025-01-09 19:05:57 -0500

    Create base of reverseString function Add string parameter Add reversedString variable Add array for string using split function Use for loop to add characters from string to reversedString variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = "";
+  const myArray = string.split(" ");
+  for (i = string.length; i >= 0; i--) {
+    reversedString += string.charAt(i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
