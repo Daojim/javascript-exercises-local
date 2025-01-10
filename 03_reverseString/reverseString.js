c7eb32ba3e376a3576aa5908f0179d1106263485
Daojim <jimmyddao@gmail.com>
2025-01-09 19:07:49 -0500

    Remove array variable due to not using it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
 const reverseString = function (string) {
   let reversedString = "";
-  const myArray = string.split(" ");
   for (i = string.length; i >= 0; i--) {
     reversedString += string.charAt(i);
   }
