778e0107e7b81ee4c058ed866007e28c9822630a
Daojim <jimmyddao@gmail.com>
2025-01-09 22:56:20 -0500

    Create removeFromArray function Add array and values parameters Create newArray variable newArray filters values parameters return newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, values) {
+  let newArray = Array.from(array);
+  newArray = newArray.filter((e) => e !== values);
+  return newArray;
 };
 
 // Do not edit below this line
