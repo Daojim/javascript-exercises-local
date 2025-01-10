d78621be61b61c40277293d7236ea8b167510809
Daojim <jimmyddao@gmail.com>
2025-01-09 23:40:36 -0500

    Add validation for integer parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,9 @@ const sumAll = function (num1, num2) {
   if (num1 < 0 || num1 < 0) {
     return "ERROR";
   }
+  if (Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+    return "ERROR";
+  }
   if (num1 < num2) {
     for (i = num1; i <= num2; i++) {
       total += i;
