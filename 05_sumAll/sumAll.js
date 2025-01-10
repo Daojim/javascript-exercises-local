4f85971acb3ede5c86b4b53e85f2e8ed773311e2
Daojim <jimmyddao@gmail.com>
2025-01-09 23:32:53 -0500

    Add else condition if num1 is greater than num2 Add validation for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,16 @@
 const sumAll = function (num1, num2) {
   let total = 0;
-  for (i = num1; i <= num2; i++) {
-    total += i;
+  if (num1 < 0 || num1 < 0) {
+    return "ERROR";
+  }
+  if (num1 < num2) {
+    for (i = num1; i <= num2; i++) {
+      total += i;
+    }
+  } else {
+    for (i = num1; i >= num2; i--) {
+      total += i;
+    }
   }
   return total;
 };
