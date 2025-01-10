29ae9b98ce46bdaf19dccb06059f499f29a91c0f
Daojim <jimmyddao@gmail.com>
2025-01-10 05:16:02 -0500

    Create functions with base conversion formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  fTemp = (fahrenheit - 32) * (5 / 9);
+  return fTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  cTemp = celcius * (9 / 5) + 32;
+  return cTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
