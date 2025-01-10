93a5a918ad0054f659af6c07475ee63cce7df723
Daojim <jimmyddao@gmail.com>
2025-01-10 05:08:58 -0500

    Add nested ifs with conditionals to fit exercise Function leapYears passes all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function (year) {
   if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+      } else return false;
+    }
     return true;
   } else {
     return false;
