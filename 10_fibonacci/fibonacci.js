1d237de230bb9378cc74162c33709fc0c2966522
Daojim <jimmyddao@gmail.com>
2025-01-24 04:23:43 -0500

    Add parseInt for number to accept strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,15 +1,15 @@
 const fibonacci = function (number) {
-  if (number < 0) {
+  if (parseInt(number) < 0) {
     return "OOPS";
   }
   fiboArray = [0, 1];
 
-  for (i = 0; i < number; i++) {
+  for (i = 0; i < parseInt(number); i++) {
     let lastIndex = fiboArray.length - 1;
     fiboArray.push(fiboArray[lastIndex] + fiboArray[lastIndex - 1]);
   }
   console.log(fiboArray);
-  return fiboArray[number];
+  return fiboArray[parseInt(number)];
 };
 
 // Do not edit below this line
