0d25fca48e2801a4215c894a83f1e8ca0153609b
Daojim <jimmyddao@gmail.com>
2025-01-24 04:45:33 -0500

    Create getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const newArray = books.map((books) => `${books.title}`);
+  return newArray;
 };
 
 // Do not edit below this line
