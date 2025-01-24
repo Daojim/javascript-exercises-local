75fc5434306823086f5b23fafdb8fb2966345a22
Daojim <jimmyddao@gmail.com>
2025-01-24 06:12:50 -0500

    Create function getAge Pass in death and birth into getAge function Return death minus birth

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,19 @@
 const findTheOldest = function (people) {
   return people.reduce((oldestPerson, currentPerson) => {
-    const oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
-    const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
-
+    const oldestAge = getAge(
+      oldestPerson.yearOfDeath,
+      oldestPerson.yearOfBirth
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfDeath,
+      currentPerson.yearOfBirth
+    );
     return oldestAge < currentAge ? currentPerson : oldestPerson;
   });
+};
+
+const getAge = function (death, birth) {
+  return death - birth;
 };
 
 // Do not edit below this line
