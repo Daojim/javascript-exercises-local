8607b1eb616800c4554b3db1ca2f9e97fe0959e4
Daojim <jimmyddao@gmail.com>
2025-01-23 19:57:51 -0500

    Create factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,11 +10,24 @@ const sum = function (numbers) {
   return numbers.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function () {};
+const multiply = function (numbers) {
+  return numbers.reduce((a, b) => a * b);
+};
 
-const power = function () {};
+const power = function (num1, num2) {
+  return num1 ** num2;
+};
 
-const factorial = function () {};
+const factorial = function (num) {
+  let total = num;
+  if (num === 0) {
+    return 1;
+  }
+  for (i = num - 1; i >= 1; i--) {
+    total = total * i;
+  }
+  return total;
+};
 
 // Do not edit below this line
 module.exports = {
