1ed017c3e4957786f98daaa665a912fffcac4c0c
Daojim <jimmyddao@gmail.com>
2025-01-24 06:03:31 -0500

    Create findTheOldest function oldestPerson is the accumulator currentPerson is current item in the array Return the person to accumulator if they are older

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
