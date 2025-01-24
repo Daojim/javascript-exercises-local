96c9bf497e2701c7dcc407052d628ea7501eabae
Daojim <jimmyddao@gmail.com>
2025-01-23 22:22:49 -0500

    Create palindrome function Takes in string Replace punctuation marks with nothing Create array from string Create middle number variable and round down Create variable for last index of array Loop up to the middle number

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const newStr = string.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  const array = Array.from(newStr);
 
+  const middleNumber = Math.floor(array.length / 2);
+
+  const lastIndex = array.length - 1;
+
+  for (i = 0; i < middleNumber; i++) {
+    console.log(i + " = " + array[i]);
+    console.log(lastIndex - i + " = " + array[lastIndex - i]);
+    if (array[i] !== array[lastIndex - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
